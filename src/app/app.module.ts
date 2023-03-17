c7cfe6e6d0636894aade1862083b402df8d6b3ff
CharKourkoulis <cult_bob@hotmail.com>
2023-03-17 16:05:09 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumsListComponent } from './albums-list/albums-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      AlbumsListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
