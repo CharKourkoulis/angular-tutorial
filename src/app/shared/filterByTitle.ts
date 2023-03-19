2bbdea294d4c9daf13e8543c834208cbfa55608e
CharKourkoulis <cult_bob@hotmail.com>
2023-03-19 14:14:08 +0200

    fix: filterByTitle method null checks

--- src/app/shared/filterByTitle.ts
+++ src/app/shared/filterByTitle.ts
@@ -2,7 +2,6 @@ import { Criteria } from "app/interfaces/criteria";
 
 export function filterByTitle<T extends object>(collection: Array<T>, criteria: Criteria):Array<T> {
   return collection.filter(item =>
-    (item as any).title?.toLowerCase()
-    .indexOf(criteria?.title?.toLowerCase() ?? '') != -1);
+    (item as any)?.title?.toLowerCase()?.indexOf(criteria?.title?.toLowerCase() ?? '') != -1);
 }
 
