d196986dc2bd5fe62ca1336283ba77bc31885f6e
CharKourkoulis <cult_bob@hotmail.com>
2023-03-21 16:08:49 +0200

    cleanup

--- src/app/services/albums.service.ts
+++ src/app/services/albums.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, of } from 'rxjs';
+import { Album } from 'app/interfaces/album';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import {albums} from '../data/data';
 import { Criteria } from '../interfaces/criteria';
 
@@ -8,9 +9,9 @@ import { Criteria } from '../interfaces/criteria';
 })
 export class AlbumsService {
 
-  albums$ = of(albums); //since there is no back-end, we created a dummy array with some data of type Album.
+  albums$: Observable<Album[]> = of(albums); //since there is no back-end, we created a dummy array with some data of type Album.
   private filterCriteriaSubject = new BehaviorSubject<Criteria>({title: ''});
-  filterCriteria$ = this.filterCriteriaSubject.asObservable();
+  filterCriteria$: Observable<Criteria> = this.filterCriteriaSubject.asObservable();
 
 constructor() { }
 
